d5ff9a02897b523c6b698dbe1e80bedc461c2335
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-01-04 13:56:36 -0800

    Add mee/itim.h
    Header for manipulating the presence of code in the ITIM
    
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

---
+++ mee/itim.h
@@ -0,0 +1,9 @@
+/* Copyright 2018 SiFive, Inc */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#ifndef MEE__ITIM_H
+#define MEE__ITIM_H
+
+#define MEE_PLACE_IN_ITIM	__attribute__((section(".text.itim")))
+
+#endif
