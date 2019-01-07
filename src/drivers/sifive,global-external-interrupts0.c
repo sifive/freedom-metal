4e1e854be1e009cb514a51a486ef7f5d724ecca6
Bunnaroath Sou <bsou@sifive.com>
2019-01-07 15:15:44 -0800

    Add local and global external interrupt support

---
+++ src/drivers/sifive,global-external-interrupts0.c
@@ -0,0 +1,76 @@
+/* Copyright 2018 SiFive, Inc */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include <mee/io.h>
+#include <mee/shutdown.h>
+#include <mee/drivers/sifive,global-external-interrupts0.h>
+
+void __mee_driver_sifive_global_external_interrupt_init(struct mee_interrupt *global0)
+{
+    struct __mee_driver_sifive_global_external_interrupts0 *mee_global0;
+
+    mee_global0 = (struct __mee_driver_sifive_global_external_interrupts0 *)(global0);
+    if (mee_global0) {
+        struct mee_interrupt *intc = mee_global0->interrupt_parent;
+
+	/* Register its interrupts with with parent controller, aka all external to default isr */
+        for (int i = 0; i < mee_global0->num_interrupts; i++) {
+	    intc->vtable->interrupt_register(intc,
+					      mee_global0->interrupt_lines[i],
+					      NULL, global0);
+	}
+    }
+}
+
+int __mee_driver_sifive_global_external_interrupt_register(struct mee_interrupt *controller,
+                                                           int id, mee_interrupt_handler_t isr,
+                                                           void *priv)
+{
+    int rc = -1;
+    struct __mee_driver_sifive_global_external_interrupts0 *global0 =
+                              (struct __mee_driver_sifive_global_external_interrupts0 *)(controller);
+
+    if (global0 && (id != 0)) {
+        struct mee_interrupt *intc = global0->interrupt_parent;
+
+        /* Enable its interrupts with parent controller */
+        if (intc) {
+            rc = intc->vtable->interrupt_register(intc, id, isr, priv);
+        }
+    }
+    return rc;
+}
+
+int __mee_driver_sifive_global_external_interrupt_enable(struct mee_interrupt *controller, int id)
+{
+    int rc = -1;
+    struct __mee_driver_sifive_global_external_interrupts0 *global0 =
+                              (struct __mee_driver_sifive_global_external_interrupts0 *)(controller);
+
+    if (global0 && (id != 0)) {
+        struct mee_interrupt *intc = global0->interrupt_parent;
+
+        /* Enable its interrupts with parent controller */
+        if (intc) {
+            rc = intc->vtable->interrupt_enable(intc, id);
+        }
+    }
+    return rc;
+}
+
+int __mee_driver_sifive_global_external_interrupt_disable(struct mee_interrupt *controller, int id)
+{
+    int rc = -1;
+    struct __mee_driver_sifive_global_external_interrupts0 *global0 =
+                              (struct __mee_driver_sifive_global_external_interrupts0 *)(controller);
+
+    if (global0 && (id != 0)) {
+        struct mee_interrupt *intc = global0->interrupt_parent;
+
+        /* Enable its interrupts with parent controller */
+        if (intc) {
+            rc = intc->vtable->interrupt_disable(intc, id);
+        }
+    }
+    return rc;
+}
