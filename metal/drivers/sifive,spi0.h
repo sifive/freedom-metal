0f0d4b6090ece927bdfcb8dd38c4672e8f993a22
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-02-15 13:28:17 -0800

    Split SPI data into const and mutable structs
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- metal/drivers/sifive,spi0.h
+++ metal/drivers/sifive,spi0.h
@@ -16,10 +16,10 @@ struct __metal_driver_vtable_sifive_spi0 {
 
 struct __metal_driver_sifive_spi0;
 
-void __metal_driver_sifive_spi0_init(struct metal_spi *spi, int baud_rate);
-int __metal_driver_sifive_spi0_transfer(struct metal_spi *spi, struct metal_spi_config *config, size_t len, char *tx_buf, char *rx_buf);
-int __metal_driver_sifive_spi0_get_baud_rate(struct metal_spi *spi);
-int __metal_driver_sifive_spi0_set_baud_rate(struct metal_spi *spi, int baud_rate);
+void __metal_driver_sifive_spi0_init(const struct metal_spi *spi, int baud_rate);
+int __metal_driver_sifive_spi0_transfer(const struct metal_spi *spi, const struct metal_spi_config *config, size_t len, char *tx_buf, char *rx_buf);
+int __metal_driver_sifive_spi0_get_baud_rate(const struct metal_spi *spi);
+int __metal_driver_sifive_spi0_set_baud_rate(const struct metal_spi *spi, int baud_rate);
 
 __METAL_DECLARE_VTABLE(__metal_driver_vtable_sifive_spi0) = {
     .spi.init          = __metal_driver_sifive_spi0_init,
@@ -28,16 +28,20 @@ __METAL_DECLARE_VTABLE(__metal_driver_vtable_sifive_spi0) = {
     .spi.set_baud_rate = __metal_driver_sifive_spi0_set_baud_rate,
 };
 
+struct __metal_driver_sifive_spi0_data {
+    unsigned long baud_rate;
+};
+
 struct __metal_driver_sifive_spi0 {
-    struct metal_spi spi;
+    const struct metal_spi spi;
     const struct __metal_driver_vtable_sifive_spi0 *vtable;
     const struct metal_clock *clock;
     const unsigned long control_base;
     const unsigned long control_size;
-    unsigned long baud_rate;
     const struct __metal_driver_sifive_gpio0 *pinmux;
     const unsigned long pinmux_output_selector;
     const unsigned long pinmux_source_selector;
+    struct __metal_driver_sifive_spi0_data *data;
 };
 
 #endif
