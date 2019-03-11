3aeac52d153e6a68ed7897ff89e60cac6abfda8e
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-03-11 15:06:44 -0700

    Add constructor for FU540-C000 L2 cache controller
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- src/drivers/sifive,fu540-c000,l2.c
+++ src/drivers/sifive,fu540-c000,l2.c
@@ -10,16 +10,28 @@
 
 #define L2_REG_WAYENABLE        0x008
 
-static int fu540_c000_l2_get_max_ways(struct __metal_driver_sifive_fu540_c000_l2 *l2);
+static void metal_driver_sifive_fu540_c000_l2_init(void) __attribute__((constructor));
+static void metal_driver_sifive_fu540_c000_l2_init(void)
+{
+#ifdef __METAL_DT_SIFIVE_FU540_C000_L2_HANDLE
+    /* Get the handle for the L2 cache controller */
+    struct __metal_driver_sifive_fu540_c000_l2 *l2 = __METAL_DT_SIFIVE_FU540_C000_L2_HANDLE;
+    if(!l2) {
+        return;
+    }
+
+    /* Get the number of available ways per bank */
+    uint32_t ways = __METAL_ACCESS_ONCE((__metal_io_u32 *)(l2->control_base + L2_REG_CONFIG));
+    ways = ((ways & L2_CONFIG_WAYS_MASK) >> L2_CONFIG_WAYS_SHIFT);
+
+    /* Enable all the ways */
+    __metal_driver_sifive_fu540_c000_l2_init(l2, ways);
+#endif
+}
 
 void __metal_driver_sifive_fu540_c000_l2_init(struct metal_cache *l2, int ways)
 {
-    __metal_driver_sifive_fu540_c000_l2_set_enabled_ways(l2, ways);
-}
-
-static int fu540_c000_l2_get_max_ways(struct __metal_driver_sifive_fu540_c000_l2 *l2)
-{
-    return 0;
+    metal_cache_set_enabled_ways(l2, ways);
 }
 
 int __metal_driver_sifive_fu540_c000_l2_get_enabled_ways(struct metal_cache *cache)
@@ -43,7 +55,7 @@ int __metal_driver_sifive_fu540_c000_l2_set_enabled_ways(struct metal_cache *cac
     }
 
     /* We can't decrease the number of enabled ways */
-    if(__metal_driver_sifive_fu540_c000_l2_get_enabled_ways(l2) > ways) {
+    if(metal_cache_get_enabled_ways(cache) > ways) {
         return -2;
     }
 
@@ -54,7 +66,7 @@ int __metal_driver_sifive_fu540_c000_l2_set_enabled_ways(struct metal_cache *cac
     __METAL_ACCESS_ONCE((__metal_io_u32 *)(l2->control_base + L2_REG_WAYENABLE)) = value;
 
     /* Make sure the number of ways was set correctly */
-    if(__metal_driver_sifive_fu540_c000_l2_get_enabled_ways(l2) != ways) {
+    if(metal_cache_get_enabled_ways(cache) != ways) {
         return -3;
     }
 
