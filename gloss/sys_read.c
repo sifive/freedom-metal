5606bdb33c646f56052ed2295eb5497fadca06c0
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-03-27 10:15:25 -0700

    Make gloss/sys_read.c use metal_tty_getc()
    Read a single byte from the default input device
    
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- gloss/sys_read.c
+++ gloss/sys_read.c
@@ -1,9 +1,25 @@
+#include <metal/tty.h>
 #include <sys/types.h>
 #include <errno.h>
+#include <unistd.h>
 
 ssize_t
 _read(int file, void *ptr, size_t len)
 {
-  errno = ENOSYS;
-  return -1;
+  if (file != STDIN_FILENO) {
+    errno = ENOSYS;
+    return -1;
+  }
+
+  char c = metal_tty_getc();
+
+  /* If we got back -1, no default input device is available */
+  if(c == -1) {
+    errno = EBADF;
+    return -1;
+  }
+
+  *((char *) ptr) = c;
+
+  return 1;
 }
