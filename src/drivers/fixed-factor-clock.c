bbf83c1dda800d9d8d7fad9686ad99c8fe6789d3
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-04-05 12:41:29 -0700

    Add driver for fixed-factor-clock
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

---
+++ src/drivers/fixed-factor-clock.c
@@ -0,0 +1,22 @@
+/* Copyright 2018 SiFive, Inc. */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include <metal/drivers/fixed-factor-clock.h>
+#include <stddef.h>
+
+long __metal_driver_fixed_factor_clock_get_rate_hz(const struct metal_clock *gclk)
+{
+    const struct __metal_driver_fixed_factor_clock *clk = (void *)gclk;
+
+    long parent_rate = 1;
+    if(clk->parent) {
+        parent_rate = clk->parent->vtable->get_rate_hz(clk->parent);
+    }
+
+    return clk->mult * parent_rate / clk->div;
+}
+
+long __metal_driver_fixed_factor_clock_set_rate_hz(struct metal_clock *gclk, long target_hz)
+{
+    return __metal_driver_fixed_factor_clock_get_rate_hz(gclk);
+}
