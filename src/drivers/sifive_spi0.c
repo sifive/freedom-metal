a95bcc6a958e63518a01c22a61a6a682c316bb1f
Haotian Wang <haotian.wang@sifive.com>
2019-05-31 16:56:32 -0700

    Dequeue the receive FIFO after sending all data
    Previously in each iteration, one byte is written into the TX FIFO, and
    at the same time, one byte is written into the RXFIFO. Now only start
    receiving (dequeueing the RX FIFO) once the output transmission has been
    completed. Dequeueing the RX FIFO only after transmission also gives
    more buffer time for the TX FIFO to really send out the data before the
    host deasserts the CS.

--- src/drivers/sifive_spi0.c
+++ src/drivers/sifive_spi0.c
@@ -15,27 +15,33 @@
 #define METAL_SPI_SCKMODE_PHA_SHIFT   0
 #define METAL_SPI_SCKMODE_POL_SHIFT   1
 
-#define METAL_SPI_CSMODE_MASK         0x3
+#define METAL_SPI_CSMODE_MASK         3
 #define METAL_SPI_CSMODE_AUTO         0
 #define METAL_SPI_CSMODE_HOLD         2
 #define METAL_SPI_CSMODE_OFF          3
 
-#define METAL_SPI_PROTO_MASK          0x3
+#define METAL_SPI_PROTO_MASK          3
 #define METAL_SPI_PROTO_SINGLE        0
 #define METAL_SPI_PROTO_DUAL          1
 #define METAL_SPI_PROTO_QUAD          2
 
-#define METAL_SPI_ENDIAN_LSB          (1 << 2)
+#define METAL_SPI_ENDIAN_LSB          4
 
-#define METAL_SPI_DISABLE_RX          (1 << 3)
+#define METAL_SPI_DISABLE_RX          8
 
 #define METAL_SPI_FRAME_LEN_SHIFT     16
 #define METAL_SPI_FRAME_LEN_MASK      (0xF << METAL_SPI_FRAME_LEN_SHIFT)
 
 #define METAL_SPI_TXDATA_FULL         (1 << 31)
 #define METAL_SPI_RXDATA_EMPTY        (1 << 31)
-#define METAL_SPI_TXMARK_MASK         0x3
-#define METAL_SPI_TXWM                (1 << 0)
+#define METAL_SPI_TXMARK_MASK         3
+#define METAL_SPI_TXWM                1
+#define METAL_SPI_TXRXDATA_MASK	      (0xFF)
+
+#define METAL_SPI_WATERMARK_MASK	  7
+
+#define METAL_SPI_CONTROL_IO          0
+#define METAL_SPI_CONTROL_MAPPED      1
 
 #define METAL_SPI_REG(offset)   (((unsigned long)control_base + offset))
 #define METAL_SPI_REGB(offset)  (__METAL_ACCESS_ONCE((__metal_io_u8  *)METAL_SPI_REG(offset)))
@@ -101,6 +107,15 @@ static int configure_spi(struct __metal_driver_sifive_spi0 *spi, struct metal_sp
     /* Set CS line */
     METAL_SPI_REGW(METAL_SIFIVE_SPI0_CSID) = config->csid;
 
+    /* Toggle off memory-mapped SPI flash mode, toggle on programmable IO mode
+     * It seems that with this line uncommented, the debugger cannot have access 
+     * to the chip at all because it assumes the chip is in memory-mapped mode. 
+     * I have to compile the code with this line commented and launch gdb,
+     * reset cores, reset $pc, set *((int *) 0x20004060) = 0, (set the flash
+     * interface control register to programmable I/O mode) and then continue
+     * Alternative, comment out the "flash" line in openocd.cfg */
+	METAL_SPI_REGW(METAL_SPI_REG_FCTRL) = METAL_SPI_CONTROL_IO;
+
     return 0;
 }
 
@@ -110,6 +125,11 @@ int __metal_driver_sifive_spi0_transfer(struct metal_spi *gspi,
                                       char *tx_buf,
                                       char *rx_buf)
 {
+	/* Transmit and receive buffer must not be empty */
+	if (tx_buf == NULL || rx_buf == NULL) {
+		return 1;
+	}
+
     struct __metal_driver_sifive_spi0 *spi = (void *)gspi;
     long control_base = __metal_driver_sifive_spi0_control_base(gspi);
     int rc = 0;
@@ -121,32 +141,48 @@ int __metal_driver_sifive_spi0_transfer(struct metal_spi *gspi,
     }
 
     /* Hold the chip select line for all len transferred */
-    METAL_SPI_REGW(METAL_SIFIVE_SPI0_CSMODE) = METAL_SPI_CSMODE_HOLD;
+    METAL_SPI_REGW(METAL_SPI_REG_CSMODE) &= ~(METAL_SPI_CSMODE_MASK);
+	METAL_SPI_REGW(METAL_SPI_REG_CSMODE) |= METAL_SPI_CSMODE_HOLD;
+
+	/* Increase the maximum delay between two consecutive frames, in units of clock cycles 
+	 * The purpose is to prevent the spi controller from deasserting the cs pin in between
+	 * consecutive frames. By default 0 frame interval is allowed between frames. The delay
+	 * between each iteration in the for loop is more than one clock cycle. 0xA means the
+	 * maximum allowed inter-frame interval is 10 clock cycles.*/
+	/** METAL_SPI_REGW(METAL_SPI_REG_DELAY1) &= ~(0xFF << 16); */
+	/** METAL_SPI_REGW(METAL_SPI_REG_DELAY1) |= (0xA << 16); */
+
+    /* Master send bytes to the slave */
+
+	/* Set the transmit watermark register to enqueue the bytes before sending out */
+	METAL_SPI_REGW(METAL_SPI_REG_TXMARK) &= ~(METAL_SPI_WATERMARK_MASK);
+	METAL_SPI_REGW(METAL_SPI_REG_TXMARK) |= len;
+	int temp = METAL_SPI_REGW(METAL_SPI_REG_TXMARK);
 
     for(int i = 0; i < len; i++) {
-        /* Wait for TXFIFO to not be full */
-        while(METAL_SPI_REGW(METAL_SIFIVE_SPI0_TXDATA) & METAL_SPI_TXDATA_FULL) ;
 
-        /* Transfer byte */
-        if(tx_buf) {
-            METAL_SPI_REGW(METAL_SIFIVE_SPI0_TXDATA) = tx_buf[i];
-        } else {
-            METAL_SPI_REGW(METAL_SIFIVE_SPI0_TXDATA) = 0;
-        }
-
-        if(i == (len - 1)) {
-            /* On the last byte, set CSMODE to auto so that the chip select
-             * transitions back to high */
-            METAL_SPI_REGW(METAL_SIFIVE_SPI0_CSMODE) = METAL_SPI_CSMODE_AUTO;
-        }
-
-        /* Wait for RXFIFO to not be empty */
-        while((rxdata = METAL_SPI_REGW(METAL_SIFIVE_SPI0_RXDATA)) & METAL_SPI_RXDATA_EMPTY) ;
-
-        if(rx_buf) {
-            rx_buf[i] = (char) (rxdata & 0xFF);
-        }
+		/* Wait for TXFIFO to not be full */
+		while(METAL_SPI_REGW(METAL_SPI_REG_TXDATA) & METAL_SPI_TXDATA_FULL);
+	
+		/* Transfer byte */
+		METAL_SPI_REGW(METAL_SPI_REG_TXDATA) &= ~(METAL_SPI_TXRXDATA_MASK);
+	    METAL_SPI_REGW(METAL_SPI_REG_TXDATA) |= tx_buf[i];
     }
+
+	/* On the last byte, set CSMODE to auto so that the chip select transitions back to high */
+	METAL_SPI_REGW(METAL_SPI_REG_CSMODE) &= ~(METAL_SPI_CSMODE_MASK);
+
+	/* Reset the maximum interframe delay to 0 clock cycle */
+	/** METAL_SPI_REGW(METAL_SPI_REG_DELAY1) &= ~(0xFF << 16); */
+
+    /* Master receive bytes from the slave */
+    for (int i = 0; i < len; i++) {
+
+		/* Wait for RXFIFO to not be empty */
+		while((rxdata = METAL_SPI_REGW(METAL_SPI_REG_RXDATA)) & METAL_SPI_RXDATA_EMPTY);
+
+		rx_buf[i] = (char) (rxdata & METAL_SPI_TXRXDATA_MASK);
+	}
 
     return 0;
 }
@@ -190,8 +226,12 @@ static void pre_rate_change_callback(void *priv)
 
     /* Detect when the TXDATA is empty by setting the transmit watermark count
      * to zero and waiting until an interrupt is pending */
+<<<<<<< HEAD:src/drivers/sifive_spi0.c
 
     METAL_SPI_REGW(METAL_SIFIVE_SPI0_TXMARK) &= ~(METAL_SPI_TXMARK_MASK);
+=======
+    METAL_SPI_REGW(METAL_SPI_REG_TXMARK) &= ~(METAL_SPI_TXMARK_MASK);
+>>>>>>> Dequeue the receive FIFO after sending all data:src/drivers/sifive,spi0.c
 
     while((METAL_SPI_REGW(METAL_SIFIVE_SPI0_IP) & METAL_SPI_TXWM) == 0) ;
 }
