c21781b8c6fc12b2a29192a61c3fa5c290bc74db
Haotian Wang <haotian.wang@sifive.com>
2019-06-19 16:23:29 -0700

    Enabled blocking sys_read, added circular buffer
    Implemented a circular buffer that is terminated by '\r' or '\n' in
    sys_read. Characters will be stored in this buffer before read into
    actual storage. This buffer has a hardcoded wrap-around length of 127.
    More work needs to be done to deal with possible exceptions because of
    this maximum length.
    
    This circular buffer also most closely resembles various I/O functions
    when they are executed normally on a PC.
    
    Non-blocking read requires playing with fcntl and the relevant drivers.
    Will deal with that in the future.

--- gloss/sys_read.c
+++ gloss/sys_read.c
@@ -3,23 +3,80 @@
 #include <errno.h>
 #include <unistd.h>
 
+/* Parameters for the circular input buffer */
+#define TTY_BUFFER_LEN 127
+static char tty_buffer[TTY_BUFFER_LEN];
+static uint8_t start_index;
+static uint8_t end_index;
+#define increase_index(n) (n = (n + 1) % TTY_BUFFER_LEN)
+#define decrease_index(n) (n = (n + TTY_BUFFER_LEN - 1) % TTY_BUFFER_LEN)
+#define buffer_len (end_index > start_index ? \
+                                        end_index - start_index : \
+                                        end_index + TTY_BUFFER_LEN - start_index \
+                                        )
+
+static void present_buffer(void) {
+  signed char c;
+  int int_buffer;
+  while (1) {
+    metal_tty_getc(&int_buffer);
+    /* If input is nothing, do nothing */
+    if (int_buffer == -1) {
+      continue;
+    }
+    switch((c = int_buffer)) {
+      /* Entered newline character, end the buffer presentation */
+      case '\r':
+      case '\n':
+        metal_tty_putc(c);
+        metal_tty_putc_raw('\n');
+        tty_buffer[end_index] = '\n';
+        increase_index(end_index);
+        return;
+
+      /* Backspace */
+      case 010:
+      /* Del */
+      case 0177:
+        if (buffer_len) {
+          decrease_index(end_index);
+          metal_tty_putc(010);
+          metal_tty_putc(' ');
+          metal_tty_putc(010);
+        }
+        break;
+        
+      /* Other "normal" input */  
+      default:
+        tty_buffer[end_index] = c;
+        metal_tty_putc(c);
+        increase_index(end_index);
+        break;
+    }
+  }
+}
+
 ssize_t
 _read(int file, void *ptr, size_t len)
 {
-  if (file != STDIN_FILENO) {
-    errno = ENOSYS;
-    return -1;
-  }
-
-  char c = metal_tty_getc();
+  char *cptr = ptr;
 
   /* If we got back -1, no default input device is available */
-  if(c == -1) {
+  if (file != STDIN_FILENO) {
     errno = EBADF;
     return -1;
   }
+ 
+  /* Present an editable buffer */
+  present_buffer();
+  int ret_len = buffer_len;
 
-  *((char *) ptr) = c;
+  /* Store the value in the buffer into ptr */
+  while (start_index != end_index) {
+    *cptr = tty_buffer[start_index];
+    cptr++;
+    increase_index(start_index);
+  }
 
-  return 1;
+  return ret_len;
 }
--- gloss/sys_write.c
+++ gloss/sys_write.c
@@ -8,12 +8,18 @@ ssize_t
 _write(int file, const void *ptr, size_t len)
 {
   if (file != STDOUT_FILENO) {
-    errno = ENOSYS;
+    errno = EBADF;
     return -1;
   }
 
   const char *bptr = ptr;
-  for (size_t i = 0; i < len; ++i)
+  for (size_t i = 0; i < len; ++i) {
     metal_tty_putc(bptr[i]);
-  return 0;
+  }
+
+  /* Should return the number of bytes actually written,
+   * but methods tty.h and uart0.h do not indicate whether
+   * bytes are actually written, return the whole write length
+   * instead */
+  return len;
 }
