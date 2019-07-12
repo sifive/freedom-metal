50b04abba565f9018e221a4177ca763a2e141d7c
Bunnaroath Sou <bsou@sifive.com>
2019-07-11 23:55:52 -0700

    Add API to check for dcache and flush

--- src/cache.c
+++ src/cache.c
@@ -2,7 +2,33 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 
 #include <metal/cache.h>
+#include <metal/machine.h>
 
 extern inline void metal_cache_init(struct metal_cache *cache, int ways);
 extern inline int metal_cache_get_enabled_ways(struct metal_cache *cache);
 extern inline int metal_cache_set_enabled_ways(struct metal_cache *cache, int ways);
+
+int metal_dcache_l1_available(int hartid) {
+    switch (hartid) {
+    case 0:
+#ifdef __METAL_CPU_0_DCACHE_HANDLE
+        return __METAL_CPU_0_DCACHE_HANDLE;
+#endif  
+        break;
+    case 1:
+#ifdef __METAL_CPU_1_DCACHE_HANDLE
+        return __METAL_CPU_1_DCACHE_HANDLE;
+#endif
+        break;
+    }
+    return 0;
+}
+
+void metal_dcache_l1_flush(int hartid)
+{
+   if (metal_dcache_l1_available(hartid)) {
+        __asm__ __volatile__ (".word 0xfc000073" : : : "memory"); // CFLUSH.D.L1
+        __asm__ __volatile__ ("fence w ,r" ::: "memory");         // FENCE
+    }
+}
+
