3e084ab26f3a7989dd05cf4be4d28b48360d8626
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-07-25 11:17:21 -0700

    Fix type signature of nop_putc
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- src/tty.c
+++ src/tty.c
@@ -44,8 +44,8 @@ static void metal_tty_init(void)
 /* This implementation of putc doesn't actually do anything, it's just there to
  * provide a shim that eats all the characters so we can ensure that everything
  * can link to metal_tty_putc. */
-int nop_putc(unsigned char c) __attribute__((section(".text.metal.nop.putc")));
-int nop_putc(unsigned char c) { return -1; }
-int metal_tty_putc(unsigned char c) __attribute__((weak, alias("nop_putc")));
+int nop_putc(int c) __attribute__((section(".text.metal.nop.putc")));
+int nop_putc(int c) { return -1; }
+int metal_tty_putc(int c) __attribute__((weak, alias("nop_putc")));
 #warning "There is no default output device, metal_tty_putc() will throw away all input."
 #endif
