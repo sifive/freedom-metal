1d7ac0a012754afb28a19ecc513f19dd3f28a0ea
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-11-25 10:43:34 -0800

    Ringbuffer operates on bytes
    Based on Keith's suggestions:
    - put/get/peek add or remove one byte at a time
    - put_bytes and friends add or remove multiple bytes at a time
    - use a Flexible Array Member to reduce pointer dereferencing
    - store positions in buffer as indices
    
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- metal/ringbuf.h
+++ metal/ringbuf.h
@@ -12,23 +12,27 @@
  * @brief The handle for a ring buffer
  */
 struct metal_ringbuf {
-    /* _buf points at the base of the ring buffer memory */
-    void *_buf;
-    /* _start points at the ring buffer location containing the first value to
-     * be read by metal_ringbuf_get */
-    void *_start;
-    /* _end points at the first empty location after the buffer contents */
-    void *_end;
-    /* _capacity is the number of items the ring buffer can hold */
-    size_t _capacity;
-    /* _item_size is the size of each element in the ring buffer memory */
-    size_t _item_size;
-    /* _empty is 0 when the buffer is empty, 1 otherwise. This is used to
-     * differentiate
-     * the full condition from the empty condition, for which both cases have
-     * _start == _end */
-    bool _empty;
+    /* _read is the index into _buf to begin reading from the buffer */
+    uint32_t _read;
+    /* _count is the number of elements in the buffer */
+    uint32_t _count;
+    /* _capacity is the total number of bytes in the buffer */
+    uint32_t _capacity;
+    /* A Flexible Array Member for holding the contents of the ringbuffer,
+     * requires C >= C99 and must be the last element in the struct */
+    uint8_t _buf[];
 };
+
+/*!
+ * @def METAL_RINGBUF_SIZEOF
+ * @brief Get the required size of the memory to hold a ringbuf of N bytes
+ *
+ * When allocating memory for use in `metal_ringbuf_create()`, users should use
+ * this macro to determine the amount of memory to allocate for a ring buffer
+ * capacity of `capacity` bytes.
+ */
+#define METAL_RINGBUF_SIZEOF(capacity)                                         \
+    (sizeof(struct metal_ringbuf) + (capacity))
 
 /*!
  * @def METAL_RINGBUF_DECLARE
@@ -37,15 +41,23 @@ struct metal_ringbuf {
  * Ring buffers bust be declared with METAL_RINGBUF_DECLARE to
  * allocate storage for the buffer.
  */
-#define METAL_RINGBUF_DECLARE(name, capacity, item_size)                       \
-    uint8_t __metal_ringbuf_##name##_buf[capacity * item_size];                \
-    struct metal_ringbuf name = {._buf = (void *)__metal_ringbuf_##name##_buf, \
-                                 ._start =                                     \
-                                     (void *)__metal_ringbuf_##name##_buf,     \
-                                 ._end = (void *)__metal_ringbuf_##name##_buf, \
-                                 ._capacity = capacity,                        \
-                                 ._item_size = item_size,                      \
-                                 ._empty = true};
+#define METAL_RINGBUF_DECLARE(name, capacity)                                  \
+    uint8_t                                                                    \
+        __metal_ringbuf_##name[sizeof(struct metal_ringbuf) + (capacity)] = {  \
+            0,                                                                 \
+            0,                                                                 \
+            0,                                                                 \
+            0,                                                                 \
+            0,                                                                 \
+            0,                                                                 \
+            0,                                                                 \
+            0,                                                                 \
+            (0xFF & (capacity)),                                               \
+            (0xFF & ((capacity) >> 8)),                                        \
+            (0xFF & ((capacity) >> 16)),                                       \
+            (0xFF & ((capacity) >> 24))};                                      \
+    struct __attribute__((__packed__)) metal_ringbuf *name =                   \
+        (struct metal_ringbuf *)&__metal_ringbuf_##name;
 
 /*!
  * @brief Creates a ring buffer, given the memory to hold the buffer
@@ -54,25 +66,37 @@ struct metal_ringbuf {
  * runtime
  *
  * @param rb A pointer to a struct metal_ringbuf to be initialized
- * @param buf The memory for the buffer
- * @param capacity The number of items the buffer can hold
- * @param item_size the size of each item in bytes
- * @return 0 if successfully created, -EINVAL if rb or buf are NULL
+ * @param capacity The number of bytes the buffer can hold
+ * @return 0 if successfully created, -EINVAL if rb is NULL
  */
-int metal_ringbuf_create(struct metal_ringbuf *rb, void *buf, size_t capacity,
-                         size_t item_size);
+int metal_ringbuf_create(struct metal_ringbuf *rb, uint32_t capacity);
 
 /*!
- * @brief Returns the number of items in the ring buffer
+ * @brief Returns the number of bytes in the ring buffer
  *
  * WARNING: This action is not thread-safe! If your application requires
  * thread-safety, you should wrap this method in the appropriate synchronization
  * mechanism.
  *
  * @param rb The ring buffer
- * @return the number of items in the ring buffer
+ * @return the number of bytes in the ring buffer, or -EINVAL if rb is NULL
  */
-size_t metal_ringbuf_num_items(struct metal_ringbuf *rb);
+int metal_ringbuf_num_bytes(struct metal_ringbuf *rb);
+
+/*!
+ * @brief Append a byte to the ring buffer
+ *
+ * Adds a byte to the end of the ring buffer, if space exists
+ *
+ * WARNING: This action is not thread-safe! If your application requires
+ * thread-safety, you should wrap this method in the appropriate synchronization
+ * mechanism.
+ *
+ * @param rb The ring buffer
+ * @param val The byte to append
+ * @return 0 if the value is added, -ENOMEM if no space remains
+ */
+int metal_ringbuf_put(struct metal_ringbuf *rb, uint8_t val);
 
 /*!
  * @brief Append a value to the ring buffer
@@ -85,9 +109,26 @@ size_t metal_ringbuf_num_items(struct metal_ringbuf *rb);
  *
  * @param rb The ring buffer
  * @param val The value to append
+ * @param len The size of the value in bytes
  * @return 0 if the value is added, -ENOMEM if no space remains
  */
-int metal_ringbuf_put(struct metal_ringbuf *rb, const void *val);
+int metal_ringbuf_put_bytes(struct metal_ringbuf *rb, const void *val,
+                            size_t len);
+
+/*!
+ * @brief Get a byte from the ring buffer
+ *
+ * Pops a byte off the front of the ring buffer, if the buffer is not empty
+ *
+ * WARNING: This action is not thread-safe! If your application requires
+ * thread-safety, you should wrap this method in the appropriate synchronization
+ * mechanism.
+ *
+ * @param rb The ring buffer
+ * @param val Stores the popped value
+ * @return 0 if a value is popped, -ENODATA if the buffer is empty
+ */
+int metal_ringbuf_get(struct metal_ringbuf *rb, uint8_t *val);
 
 /*!
  * @brief Get a value from the ring buffer
@@ -102,7 +143,20 @@ int metal_ringbuf_put(struct metal_ringbuf *rb, const void *val);
  * @param val Stores the popped value
  * @return 0 if a value is popped, -ENODATA if the buffer is empty
  */
-int metal_ringbuf_get(struct metal_ringbuf *rb, void *val);
+int metal_ringbuf_get_bytes(struct metal_ringbuf *rb, void *val, size_t len);
+
+/*!
+ * @brief Get a byte from the ring buffer without removing it from the buffer
+ *
+ * WARNING: This action is not thread-safe! If your application requires
+ * thread-safety, you should wrap this method in the appropriate synchronization
+ * mechanism.
+ *
+ * @param rb The ring buffer
+ * @param val Stores the popped byte
+ * @return 0 if a value is peeked, -ENODATA if the buffer is empty
+ */
+int metal_ringbuf_peek(struct metal_ringbuf *rb, uint8_t *val);
 
 /*!
  * @brief Get a value from the ring buffer without removing it from the buffer
@@ -113,8 +167,9 @@ int metal_ringbuf_get(struct metal_ringbuf *rb, void *val);
  *
  * @param rb The ring buffer
  * @param val Stores the popped value
+ * @param len The size of the value in bytes
  * @return 0 if a value is peeked, -ENODATA if the buffer is empty
  */
-int metal_ringbuf_peek(struct metal_ringbuf *rb, void *val);
+int metal_ringbuf_peek_bytes(struct metal_ringbuf *rb, void *val, size_t len);
 
 #endif /* METAL__RINGBUF_H */
--- src/ringbuf.c
+++ src/ringbuf.c
@@ -6,141 +6,113 @@
 
 #include <metal/ringbuf.h>
 
-int metal_ringbuf_create(struct metal_ringbuf *rb, void *buf, size_t capacity,
-                         size_t item_size) {
-    if (rb == NULL || buf == NULL) {
-        return -EINVAL;
-    }
-
-    rb->_buf = buf;
-    rb->_start = buf;
-    rb->_end = buf;
-    rb->_capacity = capacity;
-    rb->_item_size = item_size;
-    rb->_empty = true;
-
-    return 0;
-}
-
-size_t metal_ringbuf_num_items(struct metal_ringbuf *rb) {
+int metal_ringbuf_create(struct metal_ringbuf *rb, uint32_t capacity) {
     if (rb == NULL) {
         return -EINVAL;
     }
-    if (rb->_start > rb->_end) {
-        /*
-         *  size = 10
-         *  empty = 0
-         *  address  0 1 2 3 4 5 6 7 8 9
-         *  buf     | |a|b|c|d| | | | | |
-         *  start      ^
-         *  end                ^
-         *  len = (end - start)
-         *      = (5 - 1)
-         *      = 4
-         */
-        return (rb->_capacity - ((rb->_start - rb->_end) / rb->_item_size));
-    } else if (rb->_start < rb->_end) {
-        /*
-         *  size = 10
-         *  empty = 0
-         *  address  0 1 2 3 4 5 6 7 8 9
-         *  buf     |d| | | | | | |a|b|c|
-         *  start                  ^
-         *  end        ^
-         *  len = size - (start - end)
-         *      = 10 - (7 - 1)
-         *      = 4
-         */
-        return ((rb->_end - rb->_start) / rb->_item_size);
-    } else if (!rb->_empty) {
-        /*
-         *  size = 10
-         *  empty = 0
-         *  address  0 1 2 3 4 5 6 7 8 9
-         *  buf     |d|e|f|g|h|i|j|a|b|c|
-         *  start                  ^
-         *  end                    ^
-         *  len = size
-         */
-        return rb->_capacity;
-    }
-    /*
-     *  size = 10
-     *  empty = 1
-     *  address  0 1 2 3 4 5 6 7 8 9
-     *  buf     | | | | | | | | | | |
-     *  start                  ^
-     *  end                    ^
-     *  len = 0
-     */
+
+    rb->_read = 0;
+    rb->_count = 0;
+    rb->_capacity = capacity;
+
     return 0;
 }
 
-int metal_ringbuf_put(struct metal_ringbuf *rb, const void *val) {
-    if (rb == NULL || val == NULL) {
+int metal_ringbuf_num_bytes(struct metal_ringbuf *rb) {
+    if (rb == NULL) {
         return -EINVAL;
     }
-    size_t len = metal_ringbuf_num_items(rb);
-    if (len == rb->_capacity) {
+    return rb->_count;
+}
+
+int metal_ringbuf_put(struct metal_ringbuf *rb, uint8_t val) {
+    if (rb == NULL) {
+        return -EINVAL;
+    }
+    if (rb->_count == rb->_capacity) {
         /* buffer is full */
         return -ENOMEM;
     }
 
-    /* write to the buffer */
-    memcpy(rb->_end, val, rb->_item_size);
+    rb->_buf[(rb->_read + rb->_count) % rb->_capacity] = val;
+    rb->_count += 1;
 
-    if (len == 0) {
-        /* mark the buffer as no longer empty */
-        rb->_empty = false;
+    return 0;
+}
+
+int metal_ringbuf_put_bytes(struct metal_ringbuf *rb, const void *val,
+                            size_t len) {
+    if (rb == NULL) {
+        return -EINVAL;
+    }
+    if ((rb->_count + len) > rb->_capacity) {
+        /* buffer doesn't have enough space */
+        return -ENOMEM;
     }
 
-    /* increment the end pointer, wrapping if we've hit the end of the buffer */
-    if (rb->_end == (rb->_buf + ((rb->_capacity - 1) * rb->_item_size))) {
-        rb->_end = rb->_buf;
-    } else {
-        rb->_end += rb->_item_size;
+    const uint8_t *arr = (const uint8_t *)val;
+
+    for (size_t i = 0; i < len; i++) {
+        metal_ringbuf_put(rb, arr[i]);
     }
 
     return 0;
 }
 
-int metal_ringbuf_get(struct metal_ringbuf *rb, void *val) {
+int metal_ringbuf_get(struct metal_ringbuf *rb, uint8_t *val) {
     if (rb == NULL || val == NULL) {
         return -EINVAL;
     }
-    if (rb->_empty) {
+    if (rb->_count == 0) {
         return -ENODATA;
     }
 
-    /* read from the buffer */
-    memcpy(val, rb->_start, rb->_item_size);
+    *val = rb->_buf[rb->_read];
+    rb->_read = (rb->_read + 1) % rb->_capacity;
+    rb->_count -= 1;
 
-    /* increment the start pointer, wrapping if we've hit the end of the buffer
-     */
-    if (rb->_start == (rb->_buf + ((rb->_capacity - 1) * rb->_item_size))) {
-        rb->_start = rb->_buf;
-    } else {
-        rb->_start += rb->_item_size;
+    return 0;
+}
+
+int metal_ringbuf_get_bytes(struct metal_ringbuf *rb, void *val, size_t len) {
+    if (rb == NULL || val == NULL) {
+        return -EINVAL;
+    }
+    if (rb->_count < len) {
+        return -ENODATA;
     }
 
-    if (rb->_start == rb->_end) {
-        /* mark the buffer as now empty */
-        rb->_empty = true;
+    for (size_t i = 0; i < len; i++) {
+        metal_ringbuf_get(rb, (uint8_t *)(val + i));
     }
 
     return 0;
 }
 
-int metal_ringbuf_peek(struct metal_ringbuf *rb, void *val) {
+int metal_ringbuf_peek(struct metal_ringbuf *rb, uint8_t *val) {
     if (rb == NULL || val == NULL) {
         return -EINVAL;
     }
-    if (rb->_empty) {
+    if (rb->_count == 0) {
         return -ENODATA;
     }
 
-    /* read from the buffer */
-    memcpy(val, rb->_start, rb->_item_size);
+    *val = rb->_buf[rb->_read];
+
+    return 0;
+}
+
+int metal_ringbuf_peek_bytes(struct metal_ringbuf *rb, void *val, size_t len) {
+    if (rb == NULL || val == NULL) {
+        return -EINVAL;
+    }
+    if (rb->_count < len) {
+        return -ENODATA;
+    }
+
+    for (size_t i = 0; i < len; i++) {
+        *((uint8_t *)val + i) = rb->_buf[(rb->_read + i) & rb->_capacity];
+    }
 
     return 0;
 }
