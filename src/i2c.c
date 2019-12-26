28d854374deae513523a10d0e5e27ba261ca13c6
Yi-Hsiu, Hsu <yihsiu.hsu@sifive.com>
2019-12-26 00:24:12 -0800

    Fix warnings as error issues when following options applied
    -std=c99 -Wall -Wextra -Werror -Wpedantic -Werror -Wno-unused-parameter

--- src/i2c.c
+++ src/i2c.c
@@ -19,7 +19,8 @@ extern inline int metal_i2c_get_baud_rate(struct metal_i2c *i2c);
 extern inline int metal_i2c_set_baud_rate(struct metal_i2c *i2c, int baud_rate);
 
 struct metal_i2c *metal_i2c_get_device(int device_num) {
-    if (device_num >= __METAL_DT_MAX_I2CS) {
+    if (device_num >= __METAL_DT_MAX_I2CS ||
+        device_num < 0) {
         return NULL;
     }
 
