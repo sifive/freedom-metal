01096cd66d9600e06db1f2bcf8f11ff5e415658b
NagaVenkataRajesh Gubba <nagavenkatarajesh.gubba@sifive.com>
2020-04-20 15:49:14 +0530

    single memtx successful, remaining failure

---
+++ metal/dma.h
@@ -0,0 +1,131 @@
+/* Copyright 2020 SiFive, Inc */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#ifndef METAL__DMA_H
+#define METAL__DMA_H
+
+struct metal_dma;
+
+union jobcontrol {
+    struct {
+        unsigned int src_size: 3;
+        unsigned int dst_size: 3;
+        unsigned int src_inc: 1;
+        unsigned int dst_inc: 1;
+        unsigned int num_minus_1: 15;
+        unsigned int rcount: 4;
+        unsigned int done_ie: 1;
+        unsigned int error_ie: 1;
+        unsigned int trigger_mode: 1;
+    } fields;
+    unsigned int bits;
+};
+
+struct job {
+    union jobcontrol control;
+    unsigned int src_end_ptr;
+    unsigned int dst_end_ptr;    
+};
+
+/*! @brief The configuration for a DMA transfer */
+struct metal_dma_chan_config {
+#if 0
+    unsigned int *src_addr;
+    unsigned int *dest_addr;
+    unsigned int bufsize;
+    unsigned int tx_size;
+
+
+    /*! @brief The protocol for the DMA transfer */
+    enum {
+        METAL_DMA_NORMAL,
+        METAL_DMA_CIRCULAR,
+        METAL_DMA_LINKED_LIST
+    } queue_type;
+#endif
+    enum {
+        METAL_DMA_PRIORITY_LOW,
+        METAL_DMA_PRIORITY_HIGH,
+        METAL_DMA_PRIORITY_MEDIUM,
+        METAL_DMA_PRIORITY_VERY_HIGH 
+    } priority;
+
+    //unsigned int src_inc : 1;
+    //unsigned int dest_inc : 1;
+    unsigned int hw_trig_en : 1;
+    unsigned int sw_trig : 1;
+    unsigned int job_cycle: 1;    
+};
+
+enum metal_dma_state {
+    METAL_DMA_DISABLE = 0,
+    METAL_DMA_ENABLE = !METAL_DMA_DISABLE
+};
+
+struct metal_dma_vtable {    
+    int (*dma_enable)(struct metal_dma *dma, unsigned int chan, struct metal_dma_chan_config *config);
+    int (*dma_disable)(struct metal_dma *dma, unsigned int chan);
+    void (*chan_init)(struct metal_dma *dma, unsigned int chan, struct metal_dma_chan_config *config);
+    int (*chan_active)(struct metal_dma *dma, unsigned int chan);
+    void (*setup_jobques)(struct metal_dma *dma, unsigned int chan, struct metal_dma_chan_config *config);
+};
+
+
+/*! @brief A handle for a DMA device */
+struct metal_dma {
+    const struct metal_dma_vtable *vtable;
+    struct job **jobq;
+    unsigned int jobq_depth;
+    unsigned int channel_count;
+};
+
+/*! @brief Get a handle for a DMA device
+ * @param device_num The index of the desired DMA device
+ * @return A handle to the DMA device, or NULL if the device does not exist*/
+struct metal_dma *metal_dma_get_device(int device_num);
+
+/*! @brief Initialize a DMA Channel with Channel configurations 
+ * @param dma                       The handle for the DMA device to initialize
+ * @param metal_dma_channel         This is the channel number a peripheral of DMA want to use 
+ * @param metal_dma_chan_config     This is the channel configurations to be used for the channel
+ */
+inline void metal_dma_channel_init(struct metal_dma *dma, unsigned int metal_dma_channel, struct metal_dma_chan_config *config) {
+    return dma->vtable->chan_init(dma, metal_dma_channel, config);
+
+}
+
+/*! @brief Enables the DMA transfter on a pirticular channel
+ * @param dma                       The handle for the DMA device to initialize
+ * @param metal_dma_channel         This is the channel number a peripheral of DMA want to use 
+ * @param metal_dma_chan_config     This is the channel configurations to be used for the channel
+ */
+inline int metal_dma_enable(struct metal_dma *dma, unsigned int metal_dma_channel, struct metal_dma_chan_config *config) {
+    return dma->vtable->dma_enable(dma, metal_dma_channel, config);
+}
+
+/*! @brief Disables the DMA transfter on a pirticular channel
+ * @param dma                       The handle for the DMA device to initialize
+ * @param metal_dma_channel         This is the channel number a peripheral of DMA want to use  
+ */
+inline int metal_dma_disable(struct metal_dma *dma, unsigned int metal_dma_channel) {
+    return dma->vtable->dma_disable(dma, metal_dma_channel);
+}
+
+/*! @brief Disables the DMA transfter on a pirticular channel
+ * @param dma                       The handle for the DMA device to initialize
+ * @param metal_dma_channel         This is the channel number a peripheral of DMA want to use  
+ * TODO: Is this API required??? need to see once the confirmed spec is released
+ */
+inline int metal_dma_channel_active(struct metal_dma *dma, unsigned int metal_dma_channel) {
+    return dma->vtable->chan_active(dma, metal_dma_channel);
+}
+
+/*! @brief Setup the global jobqueues for all the DMA Channels
+ * @param dma                       The handle for the DMA device to initialize  
+ * @param metal_dma_chan_config     This is the channel configurations to be used for the channel
+ */
+inline void metal_dma_setup_jobques(struct metal_dma *dma, unsigned int metal_dma_channel, struct metal_dma_chan_config *config) {
+    return dma->vtable->setup_jobques(dma, metal_dma_channel, config);
+}
+
+#endif
---
+++ src/drivers/sifive_lpdma0.c
@@ -0,0 +1,162 @@
+/* Copyright 2018 SiFive, Inc */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include <metal/machine/platform.h>
+
+#ifdef METAL_SIFIVE_LPDMA0
+#include <metal/drivers/sifive_lpdma0.h>
+#include <metal/io.h>
+#include <metal/machine.h>
+#include <time.h>
+#include <stdlib.h>
+#include <assert.h>
+
+static int configure_dma(struct __metal_driver_sifive_lpdma0 *dma,
+                         struct metal_dma_chan_config *config)
+{
+	long control_base = __metal_driver_sifive_lpdma0_control_base((struct metal_dma *)dma);
+	return 0;
+}
+
+int __metal_driver_sifive_lpdma0_enable_dma(struct metal_dma *gdma,
+        									unsigned int chan,
+        									struct metal_dma_chan_config *config)
+{
+    unsigned int dma_cfg;
+	//TO DO: Need to look this below pointer, in non SRAM based
+	struct job **jobqtemp;
+	struct job *jobtemp;
+	unsigned char integ_sram_used, jobq_depth, ch_cnt_minus_1;
+	struct __metal_driver_sifive_lpdma0 *dma = (void *)gdma;
+	unsigned long control_base = __metal_driver_sifive_lpdma0_control_base((struct metal_dma *)gdma);
+
+	dma_cfg = METAL_DMA_REGW(METAL_SIFIVE_LPDMA0_CONFIG);
+	integ_sram_used = __METAL_GET_FIELD(dma_cfg, METAL_DMA_INTEG_SRAM_MASK);
+	//assert(integ_sram_used == 1);
+
+#if 0
+    jobq_depth = METAL_DMA_REGW(METAL_SIFIVE_LPDMA0_CONFIG);
+	jobq_depth = __METAL_GET_FIELD(jobq_depth, METAL_DMA_QUEUE_DEPTH_MASK);
+	jobq_depth = jobq_depth ^ 2;
+	assert(jobq_depth == 4);
+#endif
+	jobq_depth = 4;
+	
+	ch_cnt_minus_1 = __METAL_GET_FIELD(dma_cfg, METAL_DMA_CHANCNT_MINUS1_MASK);
+	ch_cnt_minus_1 += 1;
+	//assert(ch_cnt_minus_1 == 4);
+
+	if(!integ_sram_used) {
+		METAL_DMA_REGW(METAL_SIFIVE_LPDMA0_JOBQ_PTR) = (uintptr_t) jobqtemp;
+	}
+
+	gdma->jobq_depth = jobq_depth;
+	gdma->channel_count = ch_cnt_minus_1;
+
+	METAL_DMA_REGW(METAL_SIFIVE_LPDMA0_CONTROL) = METAL_DMA_CTL_EN;
+
+	return 0;
+}
+
+int __metal_driver_sifive_lpdma0_disable_dma(struct metal_dma *gdma,
+        unsigned int chan)
+{
+
+	return 0;
+}
+
+int __metal_driver_sifive_lpdma0_chan_active(struct metal_dma *gdma,
+        unsigned int chan)
+{
+	return 0;
+}
+
+
+void __metal_driver_sifive_lpdma0_setup_jobques (struct metal_dma *gdma, unsigned int chan,
+                           struct metal_dma_chan_config *config)
+{
+	struct job tempjob;
+	struct __metal_driver_sifive_lpdma0 *dma = (void *)gdma;
+	unsigned long control_base = __metal_driver_sifive_lpdma0_control_base((struct  metal_dma *)gdma);
+
+    assert(gdma->jobq[chan][0].control.fields.src_size == 4);
+	//assert(*(gdma->jobq[chan][0].src_end_ptr) == 0x05060708); gdma->jobq_depth
+
+	for (int i =0; i < 1; i++) {
+		tempjob.control.bits = gdma->jobq[chan][i].control.bits;
+		tempjob.src_end_ptr = gdma->jobq[chan][i].src_end_ptr;
+		tempjob.dst_end_ptr = gdma->jobq[chan][i].dst_end_ptr;
+
+		METAL_DMA_CHAN_ENQUE_JOBCTRL_REG(chan) = tempjob.control.bits;
+		METAL_DMA_CHAN_ENQUE_SRC_ENDPTR_REG(chan) = tempjob.src_end_ptr;
+		METAL_DMA_CHAN_ENQUE_DST_ENTPTR_REG(chan) = tempjob.dst_end_ptr;
+	}	
+}
+
+
+void __metal_driver_sifive_lpdma0_chan_init (struct metal_dma *gdma,
+                                             unsigned int chan,
+                                             struct metal_dma_chan_config *config)
+{
+	struct __metal_driver_sifive_lpdma0 *dma = (void *)gdma;
+	unsigned long control_base = __metal_driver_sifive_lpdma0_control_base((struct  metal_dma *)gdma);	
+
+	unsigned int val;
+	unsigned char channel_status = 1;
+
+	switch (config->priority) {
+	case METAL_DMA_PRIORITY_VERY_HIGH:
+		break;
+	case METAL_DMA_PRIORITY_HIGH:
+		METAL_DMA_CHAN_CONTROL_REG(chan) |= (1 << METAL_DMA_CHAN_PRIO_SHIFT);
+		break;
+	case METAL_DMA_PRIORITY_MEDIUM:
+		break;
+	case METAL_DMA_PRIORITY_LOW:
+		METAL_DMA_CHAN_CONTROL_REG(chan) &= ~(1 << METAL_DMA_CHAN_PRIO_SHIFT);
+		break;
+	default:
+		break;
+	}
+
+	if (config->hw_trig_en) {
+		METAL_DMA_CHAN_CONTROL_REG(chan) |= (1 << METAL_DMA_CHAN_HW_TRIG_SHIFT);
+	}
+	else {
+		METAL_DMA_CHAN_CONTROL_REG(chan) &= ~(1 << METAL_DMA_CHAN_HW_TRIG_SHIFT);
+	}
+
+	if (config->sw_trig) {
+		METAL_DMA_CHAN_CONTROL_REG(chan) |= (1 << METAL_DMA_CHAN_SW_TRIG_SHIFT);
+	}
+	else {
+		METAL_DMA_CHAN_CONTROL_REG(chan) &= ~(1 << METAL_DMA_CHAN_SW_TRIG_SHIFT);
+	}
+
+	if (config->job_cycle) {
+		METAL_DMA_CHAN_CONTROL_REG(chan) |= (1 << METAL_DMA_CHAN_JOB_CYCLE_SHIFT);
+	}
+	else {
+		METAL_DMA_CHAN_CONTROL_REG(chan) &= ~(1 << METAL_DMA_CHAN_JOB_CYCLE_SHIFT);
+	}
+
+	//METAL_DMA_CHAN_CONTROL_REG(chan) |= (1 << METAL_DMA_CHAN_ENABLE_SHIFT);
+#if 1
+	val = METAL_DMA_CHAN_CONTROL_REG(chan);
+	METAL_DMA_CHAN_CONTROL_REG(chan) = __METAL_SET_FIELD(val, 1, 1);
+#endif
+	while(channel_status) {
+		val = METAL_DMA_CHAN_STATUS_REG(chan);
+		channel_status = __METAL_GET_FIELD(val, METAL_DMA_CHAN_STATUS_MASK);
+	}
+}
+
+__METAL_DEFINE_VTABLE(__metal_driver_vtable_sifive_lpdma0) = {
+	.dma.dma_enable      =  __metal_driver_sifive_lpdma0_enable_dma,
+	.dma.dma_disable     =  __metal_driver_sifive_lpdma0_disable_dma,
+	.dma.chan_init       =  __metal_driver_sifive_lpdma0_chan_init,
+	.dma.chan_active     =  __metal_driver_sifive_lpdma0_chan_active,
+	.dma.setup_jobques   = __metal_driver_sifive_lpdma0_setup_jobques,
+};
+
+#endif /* METAL_SIFIVE_LPDMA0 */
