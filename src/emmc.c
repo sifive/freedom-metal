c46931cb78a32074ca65d304b924ccb397910a1b
Sachin Ghadi <sachin.ghadi@sifive.com>
2020-06-10 14:54:42 +0530

    Fixed EMMC initialisation issues. CSD and CSD_EXT read validated

--- src/emmc.c
+++ src/emmc.c
@@ -4,11 +4,14 @@
 #include <metal/machine.h>
 #include <metal/emmc.h>
 
-extern inline int metal_emmc_boot(struct metal_emmc *emmc);
+extern inline int metal_emmc_boot(struct metal_emmc *emmc,uint32_t bootpartion,uint8_t *rx_data,uint8_t size);
 extern inline int metal_emmc_init(struct metal_emmc *emmc, void *ptr);
 extern inline int metal_emmc_read_block(struct metal_emmc *emmc, long int addr, const size_t len, char *rx_buff);
 extern inline int metal_emmc_write_block(struct metal_emmc *emmc, long int addr, const size_t len, char *tx_buff);
 extern inline int metal_emmc_erase_block(struct metal_emmc *emmc, long start_addr, long int end_addr);
+
+extern inline int metal_emmc_get_boot_partition_size(struct metal_emmc *emmc,uint32_t bootpartion,uint32_t *size);
+extern inline int metal_emmc_set_boot_write_protect(struct metal_emmc *emmc,uint32_t bootpartion,uint32_t wp);
 
 struct metal_emmc *metal_emmc_get_device(const int index)
 {
