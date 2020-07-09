aebfb2304f2bc1edcad318eed7a1d5d9a6c1a95a
Nathaniel Graff <nathaniel.graff@sifive.com>
2020-07-09 13:26:56 -0700

    Remove destructors
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- metal/init.h
+++ metal/init.h
@@ -10,7 +10,6 @@
  */
 
 typedef void (*metal_constructor_t)(void);
-typedef void (*metal_destructor_t)(void);
 
 #define METAL_INIT_HIGHEST_PRIORITY 0
 #define METAL_INIT_DEFAULT_PRIORITY 5000
@@ -52,35 +51,6 @@ typedef void (*metal_destructor_t)(void);
         metal_constructor_t _##function_name##_ptr = &function_name;           \
     void function_name(void)
 
-/*! @def METAL_DESTRUCTOR
- * @brief Define a Metal destructor
- *
- * Functions defined with METAL_DESTRUCTOR will be added to the list of
- * Metal destructors. By default, these functions are called on exit by
- * the metal_fini() function.
- */
-#define METAL_DESTRUCTOR(function_name)                                        \
-    METAL_DESTRUCTOR_PRIO(function_name, METAL_INIT_DEFAULT_PRIORITY)
-
-/*! @def METAL_DESTRUCTOR_PRIO
- * @brief Define a Metal destructor with a given priority
- *
- * The priority argument should be an integer between 0 and 9999, where 0
- * is the highest priority (runs first) and 9999 is the lowest priority
- * (runs last).
- *
- * Functions defined with METAL_DESTRUCTOR_PRIO will be added to the list of
- * Metal destructors. By default, these functions are called on exit by
- * the metal_fini() function.
- */
-#define METAL_DESTRUCTOR_PRIO(function_name, priority)                         \
-    __METAL_DESTRUCTOR_PRIO(function_name, priority)
-#define __METAL_DESTRUCTOR_PRIO(function_name, priority)                       \
-    __attribute__((section(".metal.dtors"))) void function_name(void);         \
-    __attribute__((section(".metal.fini_array." #priority)))                   \
-        metal_destructor_t _##function_name##_ptr = &function_name;            \
-    void function_name(void)
-
 /*!
  * @brief Call all Metal constructors
  *
@@ -96,20 +66,6 @@ typedef void (*metal_destructor_t)(void);
 void metal_init(void);
 
 /*!
- * @brief Call all Metal destructors
- *
- * Devices supported by Metal may define Metal destructors to perform
- * initialization on exit. This function iterates over the destructors
- * and calls them in turn.
- *
- * You can add your own destructors to the functions called by metal_fini()
- * by defining functions with the METAL_DESTRUCTOR() macro.
- *
- * This function is called on exit by default by metal_fini_run().
- */
-void metal_fini(void);
-
-/*!
  * @brief Weak function to call metal_init() before main
  *
  * This function calls metal_init() before main by default. If you wish to
@@ -117,14 +73,5 @@ void metal_fini(void);
  * metal_init_run()
  */
 void metal_init_run(void);
-
-/*!
- * @brief Weak function to call metal_fini() before main
- *
- * This function calls metal_fini() at exit by default. If you wish to
- * replace or augment this call to the Metal destructors, you can redefine
- * metal_fini_run()
- */
-void metal_fini_run(void);
 
 #endif /* METAL_INIT */
--- src/init.c
+++ src/init.c
@@ -7,12 +7,10 @@
  * These function pointers are created by the linker script
  * in the .init_array section. The arrays defined by these
  * and end points are the set of functions defined by instances
- * of METAL_CONSTRUCTOR() and METAL_DESTRUCTOR().
+ * of METAL_CONSTRUCTOR().
  */
 extern metal_constructor_t metal_constructors_start;
 extern metal_constructor_t metal_constructors_end;
-extern metal_destructor_t metal_destructors_start;
-extern metal_destructor_t metal_destructors_end;
 
 void metal_init(void) {
     /* Make sure the constructors only run once */
@@ -36,37 +34,11 @@ void metal_init(void) {
     }
 }
 
-void metal_fini(void) {
-    /* Make sure the destructors only run once */
-    static int fini_done = 0;
-    if (fini_done) {
-        return;
-    }
-    fini_done = 1;
-
-    if (&metal_destructors_end <= &metal_destructors_start) {
-        return;
-    }
-
-    metal_destructor_t *funcptr = &metal_destructors_start;
-    while (funcptr != &metal_destructors_end) {
-        metal_destructor_t func = *funcptr;
-
-        func();
-
-        funcptr += 1;
-    }
-}
-
 /*
- * metal_init_run() and metal_fini_run() are marked weak so that users
- * can redefine them for their own purposes, including to no-ops
- * in the case that users don't want the metal constructors or
- * destructors to run.
+ * metal_init_run() is marked weak so that users can redefine
+ * it for their own purposes, including to no-ops in the case
+ * that users don't want the metal constructors to run.
  */
 
-void metal_init_run(void) __attribute__((weak));
+void metal_init_run(void) __attribute__((weak, constructor));
 void metal_init_run(void) { metal_init(); }
-
-void metal_fini_run(void) __attribute__((weak));
-void metal_fini_run(void) { metal_fini(); }
