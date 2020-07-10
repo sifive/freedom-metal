79073176b9d9715fbb43e505f4d39184becf79f7
Brian Grayson <brian.grayson@sifive.com>
2020-07-09 18:23:02 -0700

    tty.c: Added protection in case LTO breaks the assumption that the argument is passed in a0

--- src/tty.c
+++ src/tty.c
@@ -44,7 +44,13 @@ int nop_putc(int c) __attribute__((section(".text.metal.nop.putc")));
 // look for this instruction, and use the value in a0 as the character to be
 // printed.
 int nop_putc(int c) {
-    __asm__ volatile("slli x0,a0,0x11");
+    // The ABI states that c will be passed in a0. However, under an LTO
+    // (link-time-optimizer), it may choose to optimize in ways that would
+    // break this assumption.  We want to ensure that the passed argument is
+    // truly in a0, for easier post-processing, and so there is a single
+    // 32-bit opcode to match against.
+    // So explicitly ensure that the argument is placed into a0 first.
+    __asm__ volatile("mv a0, %0; slli x0,a0,0x11" :: "r"(c));
     return -1;
 }
 int metal_tty_putc(int c) __attribute__((weak, alias("nop_putc")));
