e62dc59dd18c679bcb5c952d9d1efb9903ea4577
Nathaniel Graff <nathaniel.graff@sifive.com>
2020-07-17 15:28:33 -0700

    Update PMP API for refactor
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- metal/pmp.h
+++ metal/pmp.h
@@ -18,7 +18,7 @@
  * can be found by reading the RISC-V Privileged Architecture Specification.
  */
 
-#include <metal/machine.h>
+#include <metal/generated/cpu.h>
 #include <stddef.h>
 
 struct metal_pmp;
@@ -61,26 +61,12 @@ struct metal_pmp_config {
 };
 
 /*!
- * @brief A handle for the PMP device
- */
-struct metal_pmp {
-    /* The minimum granularity of the PMP region. Set by metal_pmp_init */
-    uintptr_t _granularity[METAL_MAX_CORES];
-};
-
-/*!
- * @brief Get the PMP device handle
- */
-struct metal_pmp *metal_pmp_get_device(void);
-
-/*!
  * @brief Get the number of pmp regions for the hartid
  */
 int metal_pmp_num_regions(int hartid);
 
 /*!
  * @brief Initialize the PMP
- * @param pmp The PMP device handle to be initialized
  *
  * The PMP initialization routine is optional and may be called as many times
  * as is desired. The effect of the initialization routine is to attempt to set
@@ -91,124 +77,109 @@ int metal_pmp_num_regions(int hartid);
  * If any regions are fused to preset values by the implementation or locked,
  * those PMP regions will silently remain uninitialized.
  */
-void metal_pmp_init(struct metal_pmp *pmp);
+void metal_pmp_init(void);
 
 /*!
  * @brief Configure a PMP region
- * @param pmp The PMP device handle
  * @param region The PMP region to configure
  * @param config The desired configuration of the PMP region
  * @param address The desired address of the PMP region
  * @return 0 upon success
  */
-int metal_pmp_set_region(struct metal_pmp *pmp, unsigned int region,
-                         struct metal_pmp_config config, size_t address);
+int metal_pmp_set_region(unsigned int region, struct metal_pmp_config config,
+                         size_t address);
 
 /*!
  * @brief Get the configuration for a PMP region
- * @param pmp The PMP device handle
  * @param region The PMP region to read
  * @param config Variable to store the PMP region configuration
  * @param address Variable to store the PMP region address
  * @return 0 if the region is read successfully
  */
-int metal_pmp_get_region(struct metal_pmp *pmp, unsigned int region,
-                         struct metal_pmp_config *config, size_t *address);
+int metal_pmp_get_region(unsigned int region, struct metal_pmp_config *config,
+                         size_t *address);
 
 /*!
  * @brief Lock a PMP region
- * @param pmp The PMP device handle
  * @param region The PMP region to lock
  * @return 0 if the region is successfully locked
  */
-int metal_pmp_lock(struct metal_pmp *pmp, unsigned int region);
+int metal_pmp_lock(unsigned int region);
 
 /*!
  * @brief Set the address for a PMP region
- * @param pmp The PMP device handle
  * @param region The PMP region to set
  * @param address The desired address of the PMP region
  * @return 0 if the address is successfully set
  */
-int metal_pmp_set_address(struct metal_pmp *pmp, unsigned int region,
-                          size_t address);
+int metal_pmp_set_address(unsigned int region, size_t address);
 
 /*!
  * @brief Get the address of a PMP region
- * @param pmp The PMP device handle
  * @param region The PMP region to read
  * @return The address of the PMP region, or 0 if the region could not be read
  */
-size_t metal_pmp_get_address(struct metal_pmp *pmp, unsigned int region);
+size_t metal_pmp_get_address(unsigned int region);
 
 /*!
  * @brief Set the addressing mode of a PMP region
- * @param pmp The PMP device handle
  * @param region The PMP region to set
  * @param mode The PMP addressing mode to set
  * @return 0 if the addressing mode is successfully set
  */
-int metal_pmp_set_address_mode(struct metal_pmp *pmp, unsigned int region,
+int metal_pmp_set_address_mode(unsigned int region,
                                enum metal_pmp_address_mode mode);
 
 /*!
  * @brief Get the addressing mode of a PMP region
- * @param pmp The PMP device handle
  * @param region The PMP region to read
  * @return The address mode of the PMP region
  */
-enum metal_pmp_address_mode metal_pmp_get_address_mode(struct metal_pmp *pmp,
-                                                       unsigned int region);
+enum metal_pmp_address_mode metal_pmp_get_address_mode(unsigned int region);
 
 /*!
  * @brief Set the executable bit for a PMP region
- * @param pmp The PMP device handle
  * @param region The PMP region to set
  * @param X The desired value of the executable bit
  * @return 0 if the executable bit is successfully set
  */
-int metal_pmp_set_executable(struct metal_pmp *pmp, unsigned int region, int X);
+int metal_pmp_set_executable(unsigned int region, int X);
 
 /*!
  * @brief Get the executable bit for a PMP region
- * @param pmp The PMP device handle
  * @param region The PMP region to read
  * @return the value of the executable bit
  */
-int metal_pmp_get_executable(struct metal_pmp *pmp, unsigned int region);
+int metal_pmp_get_executable(unsigned int region);
 
 /*!
  * @brief Set the writable bit for a PMP region
- * @param pmp The PMP device handle
  * @param region The PMP region to set
  * @param W The desired value of the writable bit
  * @return 0 if the writable bit is successfully set
  */
-int metal_pmp_set_writeable(struct metal_pmp *pmp, unsigned int region, int W);
+int metal_pmp_set_writeable(unsigned int region, int W);
 
 /*!
  * @brief Get the writable bit for a PMP region
- * @param pmp The PMP device handle
  * @param region The PMP region to read
  * @return the value of the writable bit
  */
-int metal_pmp_get_writeable(struct metal_pmp *pmp, unsigned int region);
+int metal_pmp_get_writeable(unsigned int region);
 
 /*!
  * @brief Set the readable bit for a PMP region
- * @param pmp The PMP device handle
  * @param region The PMP region to set
  * @param R The desired value of the readable bit
  * @return 0 if the readable bit is successfully set
  */
-int metal_pmp_set_readable(struct metal_pmp *pmp, unsigned int region, int R);
+int metal_pmp_set_readable(unsigned int region, int R);
 
 /*!
  * @brief Set the readable bit for a PMP region
- * @param pmp The PMP device handle
  * @param region The PMP region to read
  * @return the value of the readable bit
  */
-int metal_pmp_get_readable(struct metal_pmp *pmp, unsigned int region);
+int metal_pmp_get_readable(unsigned int region);
 
 #endif
