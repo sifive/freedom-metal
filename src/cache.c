d5affc4f778254a4ac1bd26a6696a8428c821351
Nathaniel Graff <nathaniel.graff@sifive.com>
2020-07-22 09:46:59 -0700

    Update L1 Cache API for refactor
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- src/cache.c
+++ src/cache.c
@@ -2,113 +2,15 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 
 #include <metal/cache.h>
-#include <metal/machine.h>
+#include <metal/cpu.h>
+#include <metal/generated/cache.h>
 
-extern __inline__ void metal_cache_init(struct metal_cache *cache, int ways);
-extern __inline__ int metal_cache_get_enabled_ways(struct metal_cache *cache);
-extern __inline__ int metal_cache_set_enabled_ways(struct metal_cache *cache,
-                                                   int ways);
-
-int metal_dcache_l1_available(int hartid) {
-    switch (hartid) {
-    case 0:
-#ifdef __METAL_CPU_0_DCACHE_HANDLE
-        return __METAL_CPU_0_DCACHE_HANDLE;
-#endif
-        break;
-    case 1:
-#ifdef __METAL_CPU_1_DCACHE_HANDLE
-        return __METAL_CPU_1_DCACHE_HANDLE;
-#endif
-        break;
-    case 2:
-#ifdef __METAL_CPU_2_DCACHE_HANDLE
-        return __METAL_CPU_2_DCACHE_HANDLE;
-#endif
-        break;
-    case 3:
-#ifdef __METAL_CPU_3_DCACHE_HANDLE
-        return __METAL_CPU_3_DCACHE_HANDLE;
-#endif
-        break;
-    case 4:
-#ifdef __METAL_CPU_4_DCACHE_HANDLE
-        return __METAL_CPU_4_DCACHE_HANDLE;
-#endif
-        break;
-    case 5:
-#ifdef __METAL_CPU_5_DCACHE_HANDLE
-        return __METAL_CPU_5_DCACHE_HANDLE;
-#endif
-        break;
-    case 6:
-#ifdef __METAL_CPU_6_DCACHE_HANDLE
-        return __METAL_CPU_6_DCACHE_HANDLE;
-#endif
-        break;
-    case 7:
-#ifdef __METAL_CPU_7_DCACHE_HANDLE
-        return __METAL_CPU_7_DCACHE_HANDLE;
-#endif
-        break;
-    case 8:
-#ifdef __METAL_CPU_8_DCACHE_HANDLE
-        return __METAL_CPU_8_DCACHE_HANDLE;
-#endif
-        break;
-    }
-    return 0;
+bool metal_dcache_l1_available(void) {
+    return HART_HAS_L1_DCACHE(metal_cpu_get_current_hartid());
 }
 
-int metal_icache_l1_available(int hartid) {
-    switch (hartid) {
-    case 0:
-#ifdef __METAL_CPU_0_ICACHE_HANDLE
-        return __METAL_CPU_0_ICACHE_HANDLE;
-#endif
-        break;
-    case 1:
-#ifdef __METAL_CPU_1_ICACHE_HANDLE
-        return __METAL_CPU_1_ICACHE_HANDLE;
-#endif
-        break;
-    case 2:
-#ifdef __METAL_CPU_2_ICACHE_HANDLE
-        return __METAL_CPU_2_ICACHE_HANDLE;
-#endif
-        break;
-    case 3:
-#ifdef __METAL_CPU_3_ICACHE_HANDLE
-        return __METAL_CPU_3_ICACHE_HANDLE;
-#endif
-        break;
-    case 4:
-#ifdef __METAL_CPU_4_ICACHE_HANDLE
-        return __METAL_CPU_4_ICACHE_HANDLE;
-#endif
-        break;
-    case 5:
-#ifdef __METAL_CPU_5_ICACHE_HANDLE
-        return __METAL_CPU_5_ICACHE_HANDLE;
-#endif
-        break;
-    case 6:
-#ifdef __METAL_CPU_6_ICACHE_HANDLE
-        return __METAL_CPU_6_ICACHE_HANDLE;
-#endif
-        break;
-    case 7:
-#ifdef __METAL_CPU_7_ICACHE_HANDLE
-        return __METAL_CPU_7_ICACHE_HANDLE;
-#endif
-        break;
-    case 8:
-#ifdef __METAL_CPU_8_ICACHE_HANDLE
-        return __METAL_CPU_8_ICACHE_HANDLE;
-#endif
-        break;
-    }
-    return 0;
+bool metal_icache_l1_available(void) {
+    return HART_HAS_L1_ICACHE(metal_cpu_get_current_hartid());
 }
 
 /*!
@@ -129,8 +31,8 @@ int metal_icache_l1_available(int hartid) {
  * rs1 != x0, CFLUSH.D.L1 writes back and invalidates the L1 D$ line containing
  *            the virtual address in integer register rs1.
  */
-void metal_dcache_l1_flush(int hartid, uintptr_t address) {
-    if (metal_dcache_l1_available(hartid)) {
+void metal_dcache_l1_flush(uintptr_t address) {
+    if (metal_dcache_l1_available()) {
         if (address) {
             uintptr_t ms1 = 0, ms2 = 0;
             __asm__ __volatile__("csrr %0, mtvec \n\t"
@@ -168,8 +70,8 @@ void metal_dcache_l1_flush(int hartid, uintptr_t address) {
  * back. rs1 != x0, CDISCARD.D.L1 invalidates the L1 D$ line containing the
  * virtual address in integer register rs1, with no writes back.
  */
-void metal_dcache_l1_discard(int hartid, uintptr_t address) {
-    if (metal_dcache_l1_available(hartid)) {
+void metal_dcache_l1_discard(uintptr_t address) {
+    if (metal_dcache_l1_available()) {
         if (address) {
             uintptr_t ms1 = 0, ms2 = 0;
             __asm__ __volatile__("csrr %0, mtvec \n\t"
