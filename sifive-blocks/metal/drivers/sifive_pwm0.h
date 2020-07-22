1136c85018ecb88f6bc261c49fb7b969da0077f6
Keith Packard <keithp@keithp.com>
2020-07-22 12:06:19 -0700

    Add PWM drivers
    Update to current API.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

---
+++ sifive-blocks/metal/drivers/sifive_pwm0.h
@@ -0,0 +1,13 @@
+/* Copyright 2020 SiFive, Inc */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#ifndef METAL__DRIVERS__SIFIVE_PWM0_H
+#define METAL__DRIVERS__SIFIVE_PWM0_H
+
+#include <metal/clock.h>
+#include <metal/pwm.h>
+
+/* Max possible PWM channel count */
+#define METAL_MAX_PWM_CHANNELS 16
+
+#endif
