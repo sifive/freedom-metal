5d7b4a2dc37a0f3f8ce6ebc5462d6e0a5c3b0f31
Nandkumar Joshi <nandkumar.joshi@sifive.com>
2020-09-23 01:58:27 +0530

    Add support for L2 LIM.
    Ported from v20.08 - Nate
    
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- sifive-blocks/src/drivers/sifive_ccache0.c
+++ sifive-blocks/src/drivers/sifive_ccache0.c
@@ -5,9 +5,11 @@
 
 #ifdef METAL_SIFIVE_CCACHE0
 
+#include <assert.h>
 #include <metal/drivers/sifive_ccache0.h>
 #include <metal/init.h>
 #include <metal/io.h>
+#include <stddef.h>
 
 #define L2_CONFIG_WAYS_SHIFT 8
 #define L2_CONFIG_WAYS_MASK (0xFF << L2_CONFIG_WAYS_SHIFT)
@@ -16,17 +18,34 @@
     __METAL_ACCESS_ONCE(                                                       \
         (__metal_io_u32 *)(METAL_SIFIVE_CCACHE0_0_BASE_ADDRESS + (offset)))
 
+/* Linker symbols to calculate LIM allocated size */
+extern char metal_segment_lim_target_start, metal_segment_lim_target_end;
+
 void sifive_ccache0_init(uint32_t ways) {
     sifive_ccache0_set_enabled_ways(ways);
 }
 
 METAL_CONSTRUCTOR(init_sifive_ccache0) {
-    /* Get the number of available ways per bank */
-    const uint32_t ways = __METAL_GET_FIELD(
+    size_t lim_size = 0;
+    if (&metal_segment_lim_target_end > &metal_segment_lim_target_start) {
+        lim_size =
+            &metal_segment_lim_target_end - &metal_segment_lim_target_start;
+    }
+
+    const uint32_t available_ways = __METAL_GET_FIELD(
         L2_REGW(METAL_SIFIVE_CCACHE0_CONFIG), L2_CONFIG_WAYS_MASK);
 
-    /* Enable all the ways */
-    sifive_ccache0_init(ways);
+    const size_t way_size = METAL_SIFIVE_CCACHE0_0_CACHE_BLOCK_SIZE *
+                            METAL_SIFIVE_CCACHE0_0_CACHE_SETS *
+                            METAL_SIFIVE_CCACHE0_0_BANK_COUNT;
+
+    uint32_t ways_to_disable = 0;
+    if (way_size != 0) {
+        /* Divide lim_size by way_size, rounding up */
+        ways_to_disable = (lim_size + way_size - 1) / way_size;
+    }
+
+    sifive_ccache0_init(available_ways - ways_to_disable);
 }
 
 uint32_t sifive_ccache0_get_enabled_ways(void) {
