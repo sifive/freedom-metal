0613300eb5613801fadcf67880510dbb7a851831
Amarnath Pujari <amarnath.pujari@openfive.com>
2021-02-22 11:40:04 +0530

    OTP: add OTP support into freedom metal
    --add OTP driver with required headerfiles and .c files in freedom metal
    --add updated Makefile.am with OTP entries
    
    Signed-off-by: Amarnath Pujari <amarnath.pujari@openfive.com>

---
+++ src/otp.c
@@ -0,0 +1,19 @@
+/* Copyright 2019 SiFive, Inc */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include <metal/machine.h>
+#include <metal/otp.h>
+
+
+extern inline int metal_otp_init(struct metal_otp *otp, void *ptr);
+extern inline int metal_otp_read(struct metal_otp *otp, long int addr, const size_t len, uint8_t *rx_buff, int bits);
+extern inline int metal_otp_write(struct metal_otp *otp, long int addr, const size_t len, uint8_t *tx_buff, int bits);
+
+struct metal_otp *metal_otp_get_device(const int index)
+{
+  if (index > __METAL_DT_MAX_EMMC) {
+    return NULL;
+  }
+
+  return (struct metal_otp *) __metal_otp_table[index];
+}
